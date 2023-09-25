91840c8e3b93859f328cc2b958a73fc6e9e22489
Shimapondo <williamnamabende@gmail.com>
2023-09-25 08:03:37 -0700

    a function that prints an integer with {:d}.format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,26 +2,18 @@
 
 
 def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
-        print('{:d}'.format(value))
-        return True
-    except ValueError:
-        return False
-    except TypeError:
-        return False
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
 
-if __name__ == '__main__':
-    value = 89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = -89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = "Holberton"
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
