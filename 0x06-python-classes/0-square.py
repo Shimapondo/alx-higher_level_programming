5cb10afbeee2f90ef1bc7104c8e15ffba925afa5
Shimapondo <williamnamabende@gmail.com>
2023-09-27 13:51:44 -0700

    a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" python3 -c 'print(__import__("my_module").__doc__)'"""
+"""
+    python3 -c 'print(__import__("my_module").__doc__)'
+"""
 
 
 class Square:
     """Represent a square."""
     pass
-
-
