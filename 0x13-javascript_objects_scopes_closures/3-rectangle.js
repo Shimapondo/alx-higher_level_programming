be6a5548f738e8a7bdd3a8f145997f704ce8ed77
Shimapondo <williamnamabende@gmail.com>
2023-12-13 13:47:08 -0800

    Rectangle three

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 
-class Rectangle; {
-	constructor (w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print () {
-		for (let i = 0; i < this.height; i++) {
-			let myStr = '';
-			for (let j = 0; j < this.width; j++) {
-			}
-			console.log(myStr);
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myStr = '';
+      for (let j = 0; j < this.width; j++) {
+        myStr += 'X';
+      }
+      console.log(myStr);
+    }
+  }
 }
 
 module.exports = Rectangle;
