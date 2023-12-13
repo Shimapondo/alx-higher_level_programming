36ddb3a063ed2793fe0228564a11cb847eefdbcf
Shimapondo <williamnamabende@gmail.com>
2023-12-13 14:25:08 -0800

    prints arguments already printed

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const num = gene();
 
-function * gene () {
-  for (let i = 0; true; i++) {
-    yield i;
+exports.esrever = function (list) {
+  const iteration = Math.floor(list.length / 2);
+  const max = list.length - 1;
+  for (let i = 0; i < iteration; i++) {
+    const temp = list[i];
+    list[i] = list[max - i];
+    list[max - i] = temp;
   }
-}
-
-exports.logMe = function (item) {
-  console.log(`${num.next().value}: ${item}`);
+  return list;
 };
