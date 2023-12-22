2b459490316bdeefc9778fd5dd0a16cb39ac208d
Shimapondo <williamnamabende@gmail.com>
2023-12-22 12:47:12 -0800

    final corrections

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""link class for states table, this module has to be imported to
+    be used to map a relational database to python objects
 """
-    Start link class to table in database
-"""
-import sys
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class State(Base):
+    """the class that will be used to link to a relational database
+    from python
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
